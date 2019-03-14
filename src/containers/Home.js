fa8c1d0f0455cbe7edbe548eca89702d6e29507b
Ernest Viola <ernest.viol@gmail.com>
2019-03-14 11:15:40 -0700

    Adding styling to the text box

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
     render() {
         return (
             <div className="Home">
-                <div className="">
-                <div className="text-box"></div>
+                <div className="container">
+                    <div className="text-box">
                     Hey my name is Ernest and this is my Website.
-                    I'm currently looking for a job, if you have an opening at your company please let me know.
-
+                        I'm currently looking for a job, if you have an opening at your company please let me know.
+                    </div>
+                    
                 </div>
                 
             </div>
