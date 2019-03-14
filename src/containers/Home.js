92bdbaf51ebdf9a04768cfcccc033fd24022b698
Ernest Viola <ernest.viol@gmail.com>
2019-03-14 10:30:49 -0700

    Installed react router dom

---
+++ src/containers/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <div>
+                    This is where I'll have my text go.
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+export default Home;
