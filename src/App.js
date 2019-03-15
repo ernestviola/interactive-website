16d9c94e40795307ee790c141b1b5f2b0175f139
Ernest Viola <ernest.viol@gmail.com>
2019-03-15 11:01:40 -0700

    Added mobile and active on main-nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import CustomNavbar from './components/CustomNavbar'
 import Routes from './Routes';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faIgloo)
 
 class App extends Component {
   render() {
--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import './CustomNavbar.css';
 class CustomNavbar extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+
+        this.state = {
+            clicked: false,
+        };
+    }
+
+
+    handleClick() {
+        this.setState({"clicked": !this.state.clicked});
+    }
+
+
+
+
     render() {
+        // var className = this.state.clicked ? 'navbar-toggle' : 'base-state';
         return (
-            <div className="navbar">
-                
             
-            <div className="nav">
-                <ul>
-                    <li><NavLink exact to="/">Home</NavLink></li>
-                    <li><NavLink exact to="/articles">Articles</NavLink></li>
+            <nav class="navbar">
+                <span className="navbar-toggle" id="js-navbar-toggle" onClick={this.handleClick}>
+                    <FontAwesomeIcon icon={faCoffee} />
+                </span>
+                {/* <a href="#" class="logo">Ernest.</a> */}
+                <NavLink exact to="/" className="logo">Ernest.</NavLink>
+                <ul className={`main-nav ${this.state.clicked ? 'active' : ''}`} id="js-menu">
+                    <li><NavLink exact to="/" className="nav-links">Home</NavLink></li>
+                    <li><NavLink exact to="/articles" className="nav-links">Articles</NavLink></li>
                 </ul>
-            </div>
-            </div>
+            </nav>
+
         );
     }
 }
