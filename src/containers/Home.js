af1ffe1dc2f69510e1cafd215724639c638949d4
Ernest Viola <ernest.viol@gmail.com>
2019-03-24 14:33:59 -0700

    Now using react-typist

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Typist from 'react-typist';
 import './Home.css';
 
 
@@ -8,8 +9,15 @@ class Home extends Component {
             <div className="Home">
                 <div className="container">
                     <div className="text-box">
-                    Hey my name is Ernest and this is my Website.
-                        I'm currently looking for a job, if you have an opening at your company please let me know.
+                        <Typist avgTypingDelay={50} >
+                        Hey my name is Ernest and this is my Website.
+                        <Typist.Backspace count={"Hey my name is Ernest and this is my Website.".length} delay={500} avgTypingDelay={20}/>
+                        I enjoy programming as well as reading self improvement books.
+                        <Typist.Backspace count={"I enjoy programming as well as reading self improvement books.".length} delay={500} avgTypingDelay={20}/>
+                        I also really like tennis.
+                        <Typist.Backspace count={"I also really like tennis.".length} delay={500} avgTypingDelay={20}/>
+                        If you're looking for a software solution then I'm the man for you.
+                        </Typist>
                     </div>
                     
                 </div>
