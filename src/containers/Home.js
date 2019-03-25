f6a3c41f3973497ff695a852ee910e81e4e320ef
Ernest Viola <ernest.viol@gmail.com>
2019-03-24 17:13:35 -0700

    Changed the delay

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -11,11 +11,11 @@ class Home extends Component {
                     <div className="text-box">
                         <Typist avgTypingDelay={10} blink={true} >
                         Hey my name is Ernest and this is my Website.
-                        <Typist.Backspace count={"Hey my name is Ernest and this is my Website.".length} delay={500} avgTypingDelay={20}/>
+                        <Typist.Backspace count={"Hey my name is Ernest and this is my Website.".length} delay={700} avgTypingDelay={20}/>
                         I enjoy programming as well as reading self improvement books.
-                        <Typist.Backspace count={"I enjoy programming as well as reading self improvement books.".length} delay={500} avgTypingDelay={20}/>
+                        <Typist.Backspace count={"I enjoy programming as well as reading self improvement books.".length} delay={800} avgTypingDelay={20}/>
                         I also really like tennis.
-                        <Typist.Backspace count={"I also really like tennis.".length} delay={500} avgTypingDelay={20}/>
+                        <Typist.Backspace count={"I also really like tennis.".length} delay={700} avgTypingDelay={20}/>
                         If you're looking for a software solution then I'm the man for you. You can email me <a href="mailto:ernest.viol@gmail.com">here</a>.
                         </Typist>
                     </div>
