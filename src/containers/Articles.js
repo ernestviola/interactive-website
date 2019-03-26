941579b1a6188d8d11a18d260f2503f4e7b80153
Ernest Viola <ernest.viol@gmail.com>
2019-03-26 09:18:35 -0700

    Can get the markdown from a file

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import marked from 'marked';
-// import blogs from '../_posts/blog/2019-03-25-test.md';
+// import blog from '../posts/blog/2019-03-25-test.md';
 
 class Articles extends Component {
     constructor(props) {
         super(props);
-    }    
+
+        this.state = {
+            marked : ""
+        };
+    }   
+    
+    importAll(r) {
+        return r.keys().map(r);
+    }
 
     componentDidMount() {
-        
+        // const readmePath = require('../_posts/blog/2019-03-25-test.md');
+        // fetch(readmePath)
+        // .then(response => {
+        //     return response.text()
+        // })
+        // .then(text => {
+        //     this.setState({
+        //         markdown: marked(text)
+        //     })
+        // });
+        const blogs = require('../_posts');
+        console.log(typeof(blogs));
     }
 
     render() {
+        const {markdown} = this.state;
         return (
             <div>
-                This is where all of the articles are located
+                {/* <article dangerouslySetInnerHTML={{__html: markdown}}></article> */}
             </div>
         );
     }
