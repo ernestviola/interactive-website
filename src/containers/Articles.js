abeb3407a75c48ae21773fd3352a0207df04be92
Ernest Viola <ernest.viol@gmail.com>
2019-03-25 20:54:16 -0700

    Moved _posts to within src so that react can see the files

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import marked from 'marked';
+// import * as blogs from '../../_posts/blog';
 
 class Articles extends Component {
+    constructor(props) {
+        super(props);
+    }    
+
+    // componentDidMount() {
+    //     console.log(blogs);
+    // }
+
     render() {
         return (
             <div>
