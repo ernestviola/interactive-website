8f19c58ed98cad7c7f8d67b362f41bc7e02a18f4
Ernest Viola <ernest.viol@gmail.com>
2019-03-27 20:51:20 -0700

    Initialized gatsby project for version 2

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# interactive-website
-a new interactive website in order to showcase content
--- src/_posts/index.js
+++
@@ -1,5 +0,0 @@
-// function importAll(r) {
-//     return r.keys().map(r);
-//   }
-  
-//   const blogs = importAll(require.context('./blog/', false, '.md'));
